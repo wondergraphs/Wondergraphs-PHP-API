9405a815b0e1e64d303aa55a9bf650bae8f8270a
Ruben Vermeersch <ruben@savanne.be>
2011-08-04 15:31:09 +0200

    Add example for GET /users/ID/reports

---
+++ examples/get-user-reports.php
@@ -0,0 +1,8 @@
+<?php
+
+require dirname(__FILE__).'/_global.php';
+
+echo "Please enter the ID of the user whose reports you wish to retrieve: ";
+$id = trim(fgets(STDIN));
+
+print_r($api->getReportsForUser($id));
